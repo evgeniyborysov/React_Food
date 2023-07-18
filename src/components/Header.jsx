d3e572daecd527612402ddf280e0e461b971733c
evgeniy borysov <evgeniy.borysov@gmail.com>
2023-07-18 17:21:10 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+
+export const Header = () => {
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography
+						variant="h6"
+						noWrap
+						sx={{
+							flexGrow: 1,
+							textAlign: "start",
+						}}
+					>
+						React Food 🍔
+					</Typography>
+
+					<List
+						sx={{
+							display: "flex",
+						}}
+					>
+						<ListItem>
+							<Button component={Link} to="/" color="inherit">
+								Home
+							</Button>
+						</ListItem>
+						<ListItem>
+							<Button component={Link} to="about" color="inherit">
+								About
+							</Button>
+						</ListItem>
+						<ListItem>
+							<Button
+								component={Link}
+								to="contacts"
+								color="inherit"
+							>
+								Contacts
+							</Button>
+						</ListItem>
+					</List>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
+};
